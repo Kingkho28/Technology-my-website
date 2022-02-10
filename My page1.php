bc4f9a027c7e92eb50cb80135f5631e3b0bd30b4
Kingkho28 <97584998+Kingkho28@users.noreply.github.com>
2022-02-10 21:11:36 +0700

    Update My page1.php

--- My page1.php
+++ My page1.php
@@ -4,6 +4,6 @@
   </head>
   <body>
     <a href="https://facebook.com"><h1>Facebook</h1></a>
-    <a href="mypage2.php"><h1>Mypage2</h1></a>
+    <a href="My page2.php"><h1>My page2</h1></a>
   </body>
 </html>
