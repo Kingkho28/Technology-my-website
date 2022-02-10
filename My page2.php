48372cd5c48e5a646902ffb7f4958ca57f695308
Kingkho28 <97584998+Kingkho28@users.noreply.github.com>
2022-02-10 21:17:37 +0700

    Update My page2.php

--- My page2.php
+++ My page2.php
@@ -3,6 +3,6 @@
     <title>เว็บไซต์ของ กิ่งค้อ แสงคำกุล </title>
   </head>
   <body>
-    <a href="mypage1.php"><h1>mypage 1</h1></a>
+    <a href="My page1.php"><h1>My page1</h1></a>
   </body>
 </html>
