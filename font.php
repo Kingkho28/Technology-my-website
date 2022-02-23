1e61047492166527044e08c4fafdb64b7fe1f4fb
Kingkho28 <97584998+Kingkho28@users.noreply.github.com>
2022-02-23 13:50:20 +0700

    Update font.php

--- font.php
+++ font.php
@@ -6,12 +6,20 @@
         font-family: "MyFont1";
         src: url("fonttintin.ttf");
       }
-      body{
+      @font-face{
+        font-family: "MyFont2";
+        src: url("MNNTuaklom.ttf");
+      }
+      #font1{
         font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
       }
     </style>
   </head>
   <body>
-    <h1>ฟอนต์ของฉัน</h1>
+    <h1 id="font1">นางสาวกิ่งค้อ แสงคำกุล เลขที่ 28 ห้อง ม.4/12 เลขประจำตัว 59102</h1>
+    <h1 id="font2">นางสาวกิ่งค้อ แสงคำกุล เลขที่ 28 ห้อง ม.4/12 เลขประจำตัว 59102/h1>
   </body>
 </html>
