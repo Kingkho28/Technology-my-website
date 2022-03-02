b1c1411497dfc0c090863f46dd1027f7210e18dc
Kingkho28 <97584998+Kingkho28@users.noreply.github.com>
2022-03-02 13:09:42 +0700

    Update table.php

--- table.php
+++ table.php
@@ -3,7 +3,11 @@
     <title>การสร้างตาราง</title>
   </head>
   <body>
-    <table> border="2">
+    <table border="1">
+      <tr>
+        <td>คอลัมน์ที่ 1</td>
+        <td>คอลัมน์ที่ 2</td>
+        <td>คอลัมน์ที่ 3</td>
       <tr>
         <td>คอลัมน์ที่ 1</td>
         <td>คอลัมน์ที่ 2</td>
