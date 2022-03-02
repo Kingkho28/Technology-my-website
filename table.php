183535d3799070d1d11e18890e4141dbd383cfb7
Kingkho28 <97584998+Kingkho28@users.noreply.github.com>
2022-03-02 15:56:58 +0700

    Update table.php

--- table.php
+++ table.php
@@ -5,13 +5,40 @@
   <body>
     <table border="1">
       <tr>
-        <td>คอลัมน์ที่ 1</td>
-        <td>คอลัมน์ที่ 2</td>
-        <td>คอลัมน์ที่ 3</td>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/FB</th>
+      </tr>
       <tr>
-        <td>คอลัมน์ที่ 1</td>
-        <td>คอลัมน์ที่ 2</td>
-        <td>คอลัมน์ที่ 3</td>
+        <td>1</td>
+        <td>Billie Eilish</td>
+        <td><img src="https://s.isanook.com/jo/0/ui/486/2430773/175178630_192279782708009_5412702259554646808_n.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/billieeilish">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Zendaya</td>
+        <td><img src="https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/a/9/4/d/a94dc7c6ffe9a11acb8462396ee79543.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/Zendaya">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Post malone</td>
+        <td><img src="https://www.billboard.com/wp-content/uploads/2022/01/feature-post-malone-billboard-2022-bb01-eric-ryan-anderson-6-1240.jpg?w=800" width="45px"></td>
+        <td><a href="https://www.facebook.com/postmalone">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>PP Krit amnuaydechkorn</td>
+        <td><img src="https://www.newtv.co.th/images/content/ct_20201210115011352.jpg.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/ppkritt">Twitter</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>Billkin Putthipong assaratanakul</td>
+        <td><img src="https://thailandsuperstar.com/images/pix/238193543/T_2476318043941257236.jpg.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/bbillkin">Twitter</a></td>
       </tr>
     </table>
   </body>
